7c717da6e8f0ef0f3b5aaf80d679a20c679fe83b
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2021-04-24 16:10:03 -0500

    renderer base code

---
+++ src/Renderer.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <nanogui/nanogui.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+#include "Camera.h"
+#include "Lighting.h"
+// #include "Object.h"
+#include "Shader.h"
+
